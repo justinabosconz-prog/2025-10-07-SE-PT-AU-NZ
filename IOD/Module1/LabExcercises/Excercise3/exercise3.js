ec1774160eb160215fbe636cc6f205056c23dc83
justinabosconz-prog <justinabosconz@gmail.com>
2025-10-19 12:59:32 +1300

    Lab 3 Exerciseupload

---
+++ IOD/Module1/LabExcercises/Excercise3/exercise3.js
@@ -0,0 +1,12 @@
+//I tried to use class instead of id but the code didn't work. Can one function not work for both?//
+
+function changeColor1() {
+  document.getElementById("column1").style.backgroundColor = "#ff9b9b";
+  document.getElementById("heading1").innerText =
+    "Changed the first heading and the button color!";
+}
+function changeColor2() {
+  document.getElementById("column2").style.backgroundColor = "#f77e7eff";
+  document.getElementById("heading2").innerText =
+    "Changed the second heading and the button color!";
+}
