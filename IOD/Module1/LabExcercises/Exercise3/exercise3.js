e39d0e789e5d9d390d9037d1324b6d8cc3b6daac
justinabosconz-prog <justinabosconz@gmail.com>
2025-10-19 13:32:14 +1300

    corrected the file names

--- IOD/Module1/LabExcercises/Exercise3/exercise3.js
+++ IOD/Module1/LabExcercises/Exercise3/exercise3.js
@@ -3,10 +3,10 @@
 function changeColor1() {
   document.getElementById("column1").style.backgroundColor = "#ff9b9b";
   document.getElementById("heading1").innerText =
-    "Changed the first heading and the button color!";
+    "Changed the first heading and the background color!";
 }
 function changeColor2() {
   document.getElementById("column2").style.backgroundColor = "#f77e7eff";
   document.getElementById("heading2").innerText =
-    "Changed the second heading and the button color!";
+    "Changed the second heading and the background color!";
 }
