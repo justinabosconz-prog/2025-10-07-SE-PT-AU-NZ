3cc422b24b918356cfffaa935ed4306c356cdcfc
justinabosconz-prog <justinabosconz@gmail.com>
2025-10-19 13:54:41 +1300

    Exercise 4 upload

---
+++ IOD/Module1/LabExcercises/Exercise4/exercise4.js
@@ -0,0 +1,37 @@
+//Create 4 functions for the 4 main mathematical operations (-,+,/,*). Return the calculated value and then output it to the screen//
+
+// function 1 - Subtraction //
+function Subtract(a, b) {
+  return a - b;
+}
+
+// function 2 - Addition //
+function Add(a, b) {
+  return a + b;
+}
+
+// function 3 - Division //
+function Divide(a, b) {
+  return a / b;
+}
+
+// function 4 - Multiplication //
+function Multiply(a, b) {
+  return a * b;
+}
+
+let a = 6;
+let b = 3;
+
+console.log(Subtract(a, b));
+console.log(Add(a, b));
+console.log(Divide(a, b));
+console.log(Multiply(a, b));
+
+//Create a function that takes the name of a person as an argument, and prints out “Hello <name>” to the console//
+
+function namefunction(a) {
+  console.log("Hello " + a);
+}
+
+namefunction("Justina");
